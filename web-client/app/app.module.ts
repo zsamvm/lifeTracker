4b5104395fce24e4fcd72c89ee8a07b2e40fa18f
Sam Zhang <zsam@vmware.com>
2016-10-19 15:58:21 -0700

    Added routing and basic set up for dashbaord

--- web-client/app/app.module.ts
+++ web-client/app/app.module.ts
@@ -1,10 +1,21 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+/* App Root */
 import { AppComponent } from './app.component';
 
+/* Feature Modules */
+import { DashboardModule } from './dashboard/dashboard.module';
+
+/* Routing Module */
+import { AppRoutingModule } from './app-routing.module';
+
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [
+    BrowserModule,
+    AppRoutingModule,
+    DashboardModule
+  ],
   declarations:      [ AppComponent ],
   bootstrap:      [ AppComponent ]
 })
---
+++ web-client/app/dashboard/dashboard.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  template: '<h1>My Dashboard</h1>'
+})
+
+export class DashboardComponent { }
